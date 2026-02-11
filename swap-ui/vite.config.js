6b403e33ef65f0b3c1344a8f05692aacfa58e8c7
henrique <bongador@protonmail.com>
2026-02-11 15:32:04 -0600

    Added tailwindcss

--- swap-ui/vite.config.js
+++ swap-ui/vite.config.js
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'; // Correct plugin import
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     nodePolyfills({ // This is the correct plugin to use
       globals: true,
       buffer: true,
